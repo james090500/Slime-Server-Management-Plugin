b0017f72d798c039cb0704d9f63f5597561a242a
James Harrison <james@fasttortoise.co.uk>
2019-03-10 22:11:53 +0000

    Attempted to return addHeaders

--- src/main/java/com/james090500/SlimeServerManagement/Web/WebServer.java
+++ src/main/java/com/james090500/SlimeServerManagement/Web/WebServer.java
@@ -1,15 +1,22 @@
 package com.james090500.SlimeServerManagement.Web;
 
 import java.io.IOException;
+import java.util.Map;
 
 import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.NanoHTTPD.Response.Status;
 
 public class WebServer extends NanoHTTPD {
 	
 	public WebServer() throws IOException {
 		super(8080);
-		addMappings();
+		start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
         System.out.println("\nRunning! Point your browsers to http://localhost:8080/ \n");
 	}
+	
+	@Override
+	public Response serve(IHTTPSession session) {
+		return newFixedLengthResponse(Status.OK, MIME_PLAINTEXT, "Yeet");
+	}	
 
 }
